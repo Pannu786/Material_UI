e8320dac3b35e22eccb71726a783575c8a10de8a
Pannu786 <thepannu786@gmail.com>
2022-06-01 23:51:25 +0530

    all done !!

--- src/App.js
+++ src/App.js
@@ -4,59 +4,95 @@ import {
   AppBar,
   Card,
   CardActions,
-  CardContext,
+  CardContent,
   CardMedia,
   CssBaseline,
   Grid,
   Toolbar,
   Container,
   Button,
-  Box
+  Box,
+  Stack,
+  Link,
 } from '@mui/material';
 import PhotoCameraIcon from '@mui/icons-material/PhotoCamera';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
+const Copyright = () => {
+  return (
+    <Typography variant='body2' color='GrayText.secondary' align='center'>
+      {'Copyright 😛'}
+      <Link color='inherit' href='https://pannu-portfolio.vercel.app/'>
+        My Portfolio
+      </Link>
+      {new Date().getFullYear()}
+    </Typography>
+  );
+};
+
 const theme = createTheme();
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const App = () => {
   return (
-    <>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <AppBar position='relative'>
-          <Toolbar sx={{ margin: 'auto' }}>
-            <PhotoCameraIcon sx={{ mr: 2 }} />
-            <Typography variant='h6'>Photo Album</Typography>
-          </Toolbar>
-        </AppBar>
-        <main>
-          <Box
-            sx={{
-              bgcolor: 'background.paper',
-              pt: 8,
-              pb: 6,
-            }}
-          >
-            <Container maxWidth='sm'>
-              <Typography
-                variant='h2'
-                align='center'
-                color='textPrimary'
-                gutterBottom
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar position='relative'>
+        <Toolbar sx={{ margin: 'auto' }}>
+          <PhotoCameraIcon sx={{ mr: 2 }} />
+          <Typography variant='h6'>Photo Album</Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <Box
+          sx={{
+            bgcolor: 'background.paper',
+            pt: 8,
+            pb: 6,
+          }}
+        >
+          <Container maxWidth='sm'>
+            <Typography
+              variant='h2'
+              align='center'
+              color='textPrimary'
+              paragraph
+              gutterBottom
+            >
+              Photo Collection
+            </Typography>
+            <Typography
+              variant='h5'
+              align='center'
+              color='textSecondary'
+              paragraph
+            >
+              Hi fellas this is me Harpreet Singh Pannu trying to learn MateriaL
+              UI AKA MUI, lets see how it does look and I hope it will look
+              good. So thanks for stick around 😀
+            </Typography>
+            <Stack
+              sx={{ pt: 4 }}
+              direction='column'
+              spacing={2}
+              justifyContent='center'
+            >
+              <Button
+                variant='contained'
+                sx={{
+                  background: '#006064',
+                }}
               >
-                Photo Collection
-              </Typography>
-              <Typography
-                variant='h5'
-                align='center'
-                color='textSecondary'
-                paragraph
-              >
-                Hi fellas this is me Harpreet Singh Pannu trying to learn
-                MateriaL UI AKA MUI, lets see how it does look and I hope it
-                will look good. So thanks for stick around 😀
-              </Typography>
-              <div>
+                Check Out These Random Pics
+              </Button>
+              <Button variant='outlined'>Secondary action</Button>
+            </Stack>
+          </Container>
+        </Box>
+        <Container sx={{ py: 8 }} maxWidth='md'>
+          {/*//! Grid another way of doing the same thing as above but longer.*/}
+          {/* <div>
                 <Grid container spacing={2} justify='center'>
                   <Grid item>
                     <Button
@@ -74,12 +110,57 @@ const App = () => {
                     </Button>
                   </Grid>
                 </Grid>
-              </div>
-            </Container>
-          </Box>
-        </main>
-      </ThemeProvider>
-    </>
+              </div> */}
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid key={card} item xs={12} sm={6} md={4}>
+                <Card
+                  sx={{
+                    height: '100%',
+                    display: 'flex',
+                    flexDirection: 'column',
+                  }}
+                >
+                  <CardMedia
+                    component='img'
+                    sx={{ pt: '56.25%' }}
+                    image='https://source.unsplash.com/random'
+                    alt='random'
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant='h5' component='h6'>
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is a media card. You can use this section to describe
+                      the content.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size='small'>View</Button>
+                    <Button size='small'>Edit</Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+      <Box sx={{ bgcolor: 'background.paper', p: 6 }} component='footer'>
+        <Typography variant='h6' align='center' gutterBottom>
+          Footer
+        </Typography>
+        <Typography
+          variant='subtitle1'
+          align='center'
+          color='GrayText.secondary'
+          component='p'
+        >
+          Well something here to give the footer a purpose!!
+        </Typography>
+        <Copyright />
+      </Box>
+    </ThemeProvider>
   );
 };
 
